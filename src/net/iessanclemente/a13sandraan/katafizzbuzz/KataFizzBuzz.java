797df4029d58fd6a3414204a3e18b18cd9fed717
Sandra Acha <sandra.acha.nine95@gmail.com>
2016-05-25 21:27:53 +0200

    Main que genera los numeross del 1 al 100 y muestra el mensage devuelto por el metodo que comprueba si esos numeros son multiplos de 3 devolviendo Fizz, si son multiplos de 5 devolviendo Buzz y si son multiplos de ambos devolviendo FizzBuzz y test comprobando que si es multiplo de 3 devuelve Fizz

--- src/net/iessanclemente/a13sandraan/katafizzbuzz/KataFizzBuzz.java
+++ src/net/iessanclemente/a13sandraan/katafizzbuzz/KataFizzBuzz.java
@@ -2,8 +2,35 @@ package net.iessanclemente.a13sandraan.katafizzbuzz;
 
 public class KataFizzBuzz {
 
+/**
+ * Generamos los numeros del 1 al 100 llamando al metodo numeros() de la clase KataFizzBuzzMethods.
+ */
+	
 	public static void main(String[] args) {
-		
-
+		for(int i=1; i<=100;i++){
+			System.out.println(fizzbuzz(i));
+		}
+	}
+	
+/**
+ * A este metodo recibe un numero.
+ * Si el numero recibido es multiplo de 3 devolvemos 'Fizz'
+ * Si el numero recibido es multiplo de 5 devolvemos 'Buzz'
+ * Si el numero recibido es multiplo tanto de 3 como de 5 devolvemos 'FizzBuzz'
+ * Si no es multiplo devolvemos el numero.
+ * @param num: integer con un numero del 1 al 100
+ * @return : 'Fizz' o 'Buzz' o 'FizzBuzz'
+ */
+	
+	public static String fizzbuzz(int num){
+		String resultado = ""+num;
+		if(num%3 == 0 && num%5 == 0){
+			resultado = "FizzBuzz";
+		}else if(num%3 == 0){
+			resultado = "Fizz";
+		}else if(num%5 == 0){
+			resultado = "Buzz";
+		}
+		return resultado;
 	}
 }
--- tests/net/iessanclemente/a13sandraan/katafizzbuzz/tests/KataFizzBuzzTest.java
+++ tests/net/iessanclemente/a13sandraan/katafizzbuzz/tests/KataFizzBuzzTest.java
@@ -1,19 +1,32 @@
 package net.iessanclemente.a13sandraan.katafizzbuzz.tests;
 
 import static org.junit.Assert.*;
-
 import org.junit.Before;
 import org.junit.Test;
 
+import net.iessanclemente.a13sandraan.katafizzbuzz.KataFizzBuzz;
+
 public class KataFizzBuzzTest {
-
+	
+	/**
+	 * Creamos una variable para la clase KataFizzBuzz.
+	 */
+	public static KataFizzBuzz methods;
+	
+	/**
+	 * Con la variable anterior creamos un nuevo objeto.
+	 */
 	@Before
-	public void setUp() throws Exception {
+	public void setUp() throws Exception{
+		methods = new KataFizzBuzz();
 	}
 
+	/**
+	 * Test para cuando el numero es multiplo de 3, tiene que devolver 'Fizz'.
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void multiploTres(){
+		String result = methods.fizzbuzz(3);
+		assertEquals("Fizz", result);
 	}
-
 }
