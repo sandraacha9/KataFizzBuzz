8d999eade3654f3bbc6d0d7ad96f843043c4e2a6
Sandra Acha <sandra.acha.nine95@gmail.com>
2016-05-25 20:52:48 +0200

    Commit Inicial

---
+++ src/net/iessanclemente/a13sandraan/katafizzbuzz/KataFizzBuzz.java
@@ -0,0 +1,9 @@
+package net.iessanclemente.a13sandraan.katafizzbuzz;
+
+public class KataFizzBuzz {
+
+	public static void main(String[] args) {
+		
+
+	}
+}
---
+++ tests/net/iessanclemente/a13sandraan/katafizzbuzz/tests/KataFizzBuzzTest.java
@@ -0,0 +1,19 @@
+package net.iessanclemente.a13sandraan.katafizzbuzz.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class KataFizzBuzzTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
